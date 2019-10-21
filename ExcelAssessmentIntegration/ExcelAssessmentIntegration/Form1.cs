d01751a0bfcde74730ae39668c51a460add28ad9
jth1012 <JTHousek2@gmail.com>
2019-10-21 01:00:16 -0400

    initial folder creation

---
+++ ExcelAssessmentIntegration/ExcelAssessmentIntegration/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelAssessmentIntegration
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void readExcelSheet(object sender, EventArgs e)
+        {
+            //open excelApp and create the new application
+            Excel.Application excelApp;
+            excelApp = new Excel.Application();
+            //workbook 
+            Excel.Workbook excelWorkbook;
+            //worksheet 
+            Excel.Worksheet excelWorksheet;
+            //range variable
+            Excel.Range range;
+
+            string str;
+            int rowCount = 0;
+            int colCount = 0;
+
+            //make excel visible to the user
+            excelApp.Visible = true;
+
+            string workbookPath = ""; //PATH NEEDS TO BE ADDED/MANIPULATED
+            try
+            {
+                excelWorkbook = excelApp.Workbooks.Open(workbookPath,
+                   0, false, 5, "", "", false, Excel.XlPlatform.xlWindows, "",
+                   true, false, 0, true, false, false);
+
+                //holds the worksheet
+                Excel.Sheets excelSheets = excelWorkbook.Worksheets;
+
+                //read from this sheet
+                string currentSheet = "Sheet1";
+                excelWorksheet = (Excel.Worksheet)excelSheets.get_Item(currentSheet);
+
+                //find all valid cells
+                range = excelWorksheet.UsedRange;
+
+                //Number of rows
+                Console.WriteLine("Number of Rows: " + range.Rows.Count);
+
+                //Number of columns
+                Console.WriteLine("Rumber of Columns: " + range.Columns.Count);
+
+                for (rowCount = 1; rowCount <= range.Rows.Count; rowCount++)
+                {
+                    for (colCount = 1; colCount <= range.Columns.Count; colCount++)
+                    {
+                        if (range.Cells[rowCount, colCount].Value2 != null)
+                        {
+                            str = range.Cells[rowCount, colCount].Value2.ToString();
+                            Console.WriteLine("Value in cell " + rowCount + " " + colCount + " is " + str); //print each individual cell
+                        }
+
+                    }
+                }
+
+                //close
+                excelWorkbook.Close(true, null, null);
+                excelApp.Quit();
+
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("ERROR: FILE NOT READ");
+            }
+
+
+
+        }
+    }
+}
+
