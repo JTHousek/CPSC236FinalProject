1798c9a33db1f7d449988333b719f864f7c58dd1
jth1012 <JTHousek2@gmail.com>
2019-11-24 04:50:07 -0500

    calculates weighted averages, documentation, and dashes

--- ExcelAssessmentIntegration/ExcelAssessmentIntegration/objNode.cs
+++ ExcelAssessmentIntegration/ExcelAssessmentIntegration/objNode.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿//James Houseknecht, Cain Kaltenbaugh, Ethan Mooney
+//jth1012, cxk1047, eam1020
+//ExcelAssessmentIntegration
+//objNode.cs
+//Start Date: October 1, 2019
+//End Date: December 1, 2019
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,84 +15,109 @@ namespace ExcelAssessmentIntegration
 {
     public class objNode
     {
-        private string objective;
-        private double students;
-        private double maxScore;
-        private double actualScore;
-        private List<string> courseObjs = new List<string>();
-        private objNode nextObjNode;
+        private string objective;       //the name of the super objective
+        private int totalStudents;      //the total students from all course objectives
+        private double weightedAverage; //the weightedAverage of all course objectives
+        private objNode nextObjNode;    //the next objective node in the linked list
 
+        private List<int> students = new List<int>();           //list of number of students per course objective
+        private List<double> average = new List<double>();      //list of averages per course objective (actual/max)
+        private List<string> courseObjs = new List<string>();   //list of course objectives specified by an XML file read on startup
+
+        //objective node constructor
         public objNode()
         {
             objective = null;
-            students = 0.0;
-            maxScore = 0.1; //base set to 0.1 so division by zero does not occur
-            actualScore = 0.0;
+            totalStudents = 0;
+            weightedAverage = 0.0;
+            nextObjNode = null;
         }
 
-        public objNode(string objective, double students, double maxScore, double actualScore)
+        //computes the weighted average of the whole super objective
+        //from taking the number of students and the averages for each
+        //course objective
+        public void computeWeightedAverage()
         {
-            this.objective = objective;
-            this.students = students;
-            this.maxScore = maxScore;
-            this.actualScore = actualScore;
+            for (int i = 0; i < students.Count; i++)
+            {
+                weightedAverage = weightedAverage + (students[i] * average[i]);
+            }
+            weightedAverage = weightedAverage / totalStudents;
         }
 
+        //returns the weighted average
+        public double getWeightedAverage()
+        {
+            return weightedAverage;
+        }
+
+        //sets the total amount of students in the whole super objective
+        public void setTotalStudents(int totalStudents)
+        {
+            this.totalStudents = totalStudents;
+        }
+
+        //returns the total number of students
+        public int getTotalStudents()
+        {
+            return totalStudents;
+        }
+
+        //sets the name of the super objective
         public void setObjective(string objective)
         {
             this.objective = objective;
         }
 
+        //returns the super objective name
         public string getObjective()
         {
             return objective;
         }
 
-        public void setStudents(double students)
+        //adds a new course objectives' number of students to the students list
+        public void addStudents(int newStudents)
         {
-            this.students = students;
+            students.Add(newStudents);
         }
 
-        public double getStudents()
+        //returns the list of course objectives' number of students
+        public List<int> getStudents()
         {
             return students;
         }
 
-        public void setMaxScore(double maxScore)
+        //adds a new course objectives' averages to the averages list
+        public void addAverage(double newAverage)
         {
-            this.maxScore = maxScore;
+            average.Add(newAverage);
         }
 
-        public double getMaxScore()
+        //returns the list of course objectives' averages
+        public List<double> getAverage()
         {
-            return maxScore;
+            return average;
         }
 
-        public void setActualScore(double actualScore)
-        {
-            this.actualScore = actualScore;
-        }
-
-        public double getActualScore()
-        {
-            return actualScore;
-        }
-
+        //adds the course objectives attributed to the super objective through the XML sheet
         public void addCourseObj(string newCourseObj)
         {
             courseObjs.Add(newCourseObj);
         }
 
+        //returns the list of the course objectives tied to the super objective
         public List<string> getCourseObj()
         {
             return courseObjs;
         }
 
+        //sets the next node in the linked list
         public void setNextObjNode(objNode nextObjNode)
         {
             this.nextObjNode = nextObjNode;
         }
 
+        //gets the next node in the linked list
         public objNode getNextObjNode()
         {
             return nextObjNode;
--- ExcelAssessmentIntegration/ExcelAssessmentIntegration/objNodeLL.cs
+++ ExcelAssessmentIntegration/ExcelAssessmentIntegration/objNodeLL.cs
@@ -1,36 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//James Houseknecht, Cain Kaltenbaugh, Ethan Mooney
+//jth1012, cxk1047, eam1020
+//ExcelAssessmentIntegration
+//objNodeLL.cs
+//Start Date: October 1, 2019
+//End Date: December 1, 2019
 
 namespace ExcelAssessmentIntegration
 {
     public class objNodeLL
     {
-        private objNode head;
-        private objNode tail;
+        private objNode head;   //head of the objective node linked list
+        private objNode tail;   //tail of the objective node linked list
 
+        //constructor for the objective node linked list
         public objNodeLL()
         {
-
+            head = null;
+            tail = null;
         }
 
+        //returns the head of the list
         public objNode getHead()
         {
             return head;
         }
 
+        //sets the head of the list to a node
         public void setHead(objNode head)
         {
             this.head = head;
         }
 
+        //returns the tail of the list
         public objNode getTail()
         {
             return tail;
         }
 
+        //sets the tail of the list to a node
         public void setTail(objNode tail)
         {
             this.tail = tail;
